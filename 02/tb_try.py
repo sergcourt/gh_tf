f901579337b5a88b83ce4d74ef6ef03bc9347553
sergio <sergiovillani8@gmail.com>
2017-12-28 15:37:32 -0800

    tb

---
+++ 02/tb_try.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+a = tf.constant(2)
+b = tf.constant(3)
+x = tf.add(a, b)
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter('./graphs', sess.graph)
+    sess.run(x)
+
+# close the writer when you’re done using it
+writer.close()
