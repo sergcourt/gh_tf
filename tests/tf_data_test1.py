af148291829c1d7631c49366d14e216a8ff55c3e
sergcourt <sergiovillani8@gmail.com>
2018-05-22 16:22:30 -0700

    data test

--- tests/tf_data_test1.py
+++ tests/tf_data_test1.py
@@ -1,3 +1,25 @@
 import tensorflow as tf
+import numpy as np
+import pandas as pd
 
-import iris_data
+print (tf.__version__)
+
+
+x = np.random.sample((100,2))
+
+
+lino= pd.read_csv('/usr/local/google/home/sergiovillani/PycharmProjects/ghrep/gh_tf/exer/03/sales_data_training.csv', usecols=[1,2,3,4,5,6,7])
+
+lin= lino.values
+
+labels=pd.read_csv('/usr/local/google/home/sergiovillani/PycharmProjects/ghrep/gh_tf/exer/03/sales_data_training.csv',usecols=[0]  )
+
+labelline=labels.values
+
+#linuzzo= tf.data.Dataset.from_tensor_slices(lin)
+
+
+dataset=tf.data.Dataset.from_tensors((labelline,lin))
+
+print(dataset.output_shapes)
+
